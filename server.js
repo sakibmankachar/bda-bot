1189a18c967485fb4c6c605f64802555dc3dd176
Glitch (hello-express) <none>
2021-03-17 09:36:08 +0000

    ⛺️🎖 Checkpoint ./server.js:27979027/3140 ./commands/autosupport1.js:27979027/1219

--- server.js
+++ server.js
@@ -1,34 +1,57 @@
-// server.js
-// where your node app starts
+const dbd = require("dbd.js");
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const bot = new dbd.Bot({
+  token: process.env.BOT_TOKEN,
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  prefix: "&"
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+bot.onMessage();
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+const fs = require("fs");
+
+let dir = fs.readdirSync("./commands");
+
+let i = 0;
+
+handler: while (i < dir.length) {
+  const stat = fs.statSync("./commands/" + dir[i]);
+
+  if (stat.isDirectory()) {
+    const readdir = fs.readdirSync("./commands/" + dir[i]);
+
+    let nums = 0;
+
+    while (nums < readdir.length) {
+      dir.push(dir[i] + "/" + readdir[nums]);
+
+      nums++;
+    }
+
+    i++;
+
+    continue handler;
+  } else if (stat.isFile()) {
+    const command = require("./commands/" + dir[i]);
+
+    try {
+      bot[Object.keys(command)[0]](command[Object.keys(command)[0]]);
+
+      i++;
+
+      continue handler;
+    } catch (err) {
+      console.error(err.message);
+
+      delete dir[i];
+
+      continue handler;
+    }
+  } else {
+    console.error("Directory was not a Folder or File");
+
+    delete dir[i];
+
+    continue handler;
+  }
+}
