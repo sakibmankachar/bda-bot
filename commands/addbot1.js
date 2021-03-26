d3f930a7d000082204b2a61c0b77d97761b84fea
Glitch (bda-bot-sa) <none>
2021-03-26 12:34:57 +0000

    🏔🚲 Checkpoint ./commands/addbot1.js:27979027/1955

--- commands/addbot1.js
+++ commands/addbot1.js
@@ -1,21 +1,40 @@
-module.exports.awaitedCommand = {
-  name: "accept",
+module.exports.command = {
 
-  code: `$changeNickname[$getMessageVar[clinetid;$messageID];$getMessageVar[prefixadd;$messageID] | $userName[$getMessageVar[clinetid;$messageID]]]
+name: "addbot",
 
-$sendDm[$getMessageVar[userid;$messageID]; Your bot has been Acepted in! ]
+Code: `
 
-$channelSendMessage[logchannel;{title:New bot!} {description: $userTag[$getMessageVar[clinetid;$messageID]] has been acepted and added by a bot inviter!} {color:00ff00} <@$getMessageVar[clinetid;$messageID]>]
+$title[New Bot Added]
 
-$editMessage[$message[1];{title: Bot accepted!}
+$description[> User name: [$username[$authorID]#$discriminator[$authorID]]($authorAvatar?size=2048)
 
-{color: 00ff00} {description: A bot inviter has accepted this bot called **$userTag[$findUser[$getMessageVar[clinetid;$messageID]]]**}] 
+> User ID: \`$authorID`
 
-$clearReactions[$channelID;$Message[1];all]
+> Bot name: [$username[$noMentionMessage[1]]#$discriminator[$noMentionMessage[1]]]($userAvatar[$noMentionMessage[1]])
 
-$onlyIf[$memberExists[$getMessageVar[clinetid;$messageID]]==true; Hey $userTag, Add that Bot Before Trying to accept the bot!] 
+> Bot ID: `$noMentionMessage[1]`
 
-$onlyPerms[admin;manageserver;]
+> Prefix: `$noMentionMessage[2]`
 
-$suppressErrors[An Error occurred. Please try again!]`
-};
+> Invite Link
+
+> [Click Here To Invite\\](https://discord.com/oauth2/authorize?client_id=$noMentionMessage[1]&scope=bot&permissions=0&guild_id=$guildID)]
+
+$addTimestamp
+
+$color[$random[1;999999]]
+
+$onlyIf[$isNumber[$noMentionMessage[1]]!=false;That not a bot, please try again.]
+
+$onlyIf[$isBot[$noMentionMessage[1]]!=false;That not a bot, please try again.]
+
+$argsCheck[2;Usage: `&addbot (BOT ID) (PREFIX)`]
+
+$suppressErrors[Usage: `&addbot (BOT ID) (PREFIX)`]
+
+$channelSendMessage[821686049839251466;<@$authorID> Submitted a bot!]
+
+$onlyForChannels[821636076921159692;Go in <#821636076921159692> to use this command!]`
+
+}
+
