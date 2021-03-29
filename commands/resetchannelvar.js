1cc38940c66405bfad78436573ecd679bbc05363
Glitch (bda-bot-sa) <none>
2021-03-29 12:42:28 +0000

    🐥🚘 Checkpoint ./commands/resetchannelvar.js:30238128/18

--- commands/resetchannelvar.js
+++ commands/resetchannelvar.js
@@ -1,7 +1,7 @@
 module.exports.command = {
   name: "resettestchannelvar",
   code: `
-  $resetUserVar[custom;$mentioned[1;yes]]
+  $resetUserVar[custom]
   Successfully Reseted!
   $onlyAdmin[Only Admin can run this command!]`
   }
