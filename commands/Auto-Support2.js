dac38a7b95c56bb77c6fb5412425131b8c274047
Glitch (bda-sa) <none>
2021-03-30 09:49:10 +0000

    🐉🏕 Checkpoint ./commands/Auto-Support2.js:30238128/156

--- commands/Auto-Support2.js
+++ commands/Auto-Support2.js
@@ -1,13 +1,11 @@
 module.exports.command = {
-  name: "2",
-
-  nonPrefixed: true,
-
+  name: "$alwaysExecute",
+  
   code: `
   
-  $deleteCommand
-
 $onlyForChannels[821636197402804244;]
+
+$if[$message[1]==1]
 
 $dm
 
@@ -18,6 +16,8 @@ $description[You can Go <#822070833681661963> and type there \`ctc\` to create a
 ⚠️Note: This system is in beta. And might not work! Recommend to use <#821722990425276436> or <#821723042039201864> instead of a private testing channel.]
 
 $footer[This system is in beta!]
+
+$deleteCommand
 
 `
 };
