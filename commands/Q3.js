1100c0b89c9406a2ab43ff9b5b0484b7a45f72aa
Glitch (bda-sa) <none>
2021-03-31 05:10:58 +0000

    🚗🏁 Checkpoint ./commands/q5.js:30238128/38 ./commands/Q3.js:30238128/484

--- commands/Q3.js
+++ commands/Q3.js
@@ -1,8 +1,9 @@
 module.exports.command = {
   name: "q3",
-nonPrefixed: true,
+  nonPrefixed: true,
   code: `
 $deleteCommand
+$onlyForChannels[826669077422538773;]
 3.What will you do if somebody submit a bot with a wrong prefix?**Please Type Your Answer.**
 
 `
--- commands/q5.js
+++ commands/q5.js
@@ -3,6 +3,7 @@ module.exports.command = {
 nonPrefixed: true,
   code: `
 $deleteCommand
+$onlyForChannels[826669077422538773;]
 5.What will you do if a bot has no help Command?**Please type your answer**
 
  
