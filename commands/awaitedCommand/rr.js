e4303f3452a3019d85b315bf18a0a76302a23f2f
Glitch (bda-sa) <none>
2021-04-01 02:42:39 +0000

    💷🚶 Checkpoint ./commands/rrole.js:30238128/376 ./commands/awaitedCommand/rr.js:30238128/301

---
+++ commands/awaitedCommand/rr.js
@@ -0,0 +1,5 @@
+module.exports.awaitedCommand = {
+  name: "rr",
+
+  code: `$giveRoles[$authorID;827002235607187506]`
+};
---
+++ commands/rrole.js
@@ -0,0 +1,8 @@
+module.exports.command = {
+  name: "rrole",
+
+  code: `
+$reactionCollector[827003844516905022;@everyone;500d;🏆;rr;no]
+
+`
+};
